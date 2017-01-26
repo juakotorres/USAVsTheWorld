b61d825279f181ee66cfdeae55b35f38fb946099
juakotorres <juako.torres@hotmail.com>
2017-01-26 18:29:46 -0300

    libgdx proyect

---
+++ desktop/src/cl/makinolas/elfsland/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package cl.makinolas.elfsland.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import cl.makinolas.elfsland.ElfsLand;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ElfsLand(), config);
+	}
+}
